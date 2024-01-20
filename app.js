890c779ce526f468a6cad53ceabc8759f1862e61
Irf-z-sam <irfansamsi555@gmail.com>
2024-01-21 00:40:27 +0530

    Routers and Filters(Task-3

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const express = require('express');
+
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+
+const shopRoutes = require('./routes/shop');
+
+const errorRoutes = require('./routes/404');
+
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use('/add-product', (req, res, next)=>{
-    res.send('<form action ="/product" method="POST"><input type="text" name="title" placeholder="Product Title"> <input type="text" name="size" placeholder="Product Size"> <button type="submit">Add Product</button> </form>');
-});
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product',(req, res, next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/', (req, res, next)=>{
-    res.send('<h1>Hello From Express....!</h1>');
-});
+app.use(errorRoutes);
 
 app.listen(3000)
