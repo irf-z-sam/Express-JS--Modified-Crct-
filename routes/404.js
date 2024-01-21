70118146203f5b934466ebb1011fe596dad8000b
Irf-z-sam <irfansamsi555@gmail.com>
2024-01-22 00:17:23 +0530

    Serving HTML, CSS and JS (VIEWS)(Style Inside HTML)

--- routes/404.js
+++ routes/404.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 router.use((req, res, next)=>{
-    res.status(404).send('<h1>Page not found...!</h1>')
+    res.status(404).sendFile(path.join(__dirname, '../', 'views', '404.html'));
 });
 
 module.exports = router;
